7a127a5669e5b27ec524d49fd6f7cfce235d3604
dr. Simon Bálint <balladin0@gmail.com>
2020-05-06 10:32:25 +0200

    rewrite class to function

--- main/src/components/pages/PokemonCard.js
+++ main/src/components/pages/PokemonCard.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import InlineBlock from "react-inline-block";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import PokemonDetail from "./PokemonDetail";
 
-export default class PokemonCard extends Component {
-  state = {
-    actualPokemon: {},
-    sprites: {},
-  };
+const PokemonCard = (props) => {
+  const [actualPokemon, setActualPokemon] = useState({});
+  const [sprites, setSprites] = useState({});
 
-  componentDidMount() {
-    axios.get(this.props.pokemon.url).then((res) => {
-      this.setState({ actualPokemon: res.data });
+  useEffect(() => {
+    axios.get(props.pokemon.url).then((res) => {
+      setActualPokemon(res.data);
     });
-  }
+  }, []);
 
-  render() {
-    const { id, name, url } = this.props.pokemon;
-    return (
-      <div className="cards">
-        <p>{name}</p>
-        <Link
-          exact
-          path="/detail"
-          render={(props) => (
-            <PokemonDetail actualPokemon={this.state.actualPokemon} />
-          )}
-        >{`Go to ${name}'s details page`}</Link>
-      </div>
-    );
-  }
-}
+  const { id, name, url } = props.pokemon;
+  return (
+    <div className="cards">
+      <p>{name}</p>
+      <Link
+        exact
+        path="/detail"
+        render={(props) => <PokemonDetail actualPokemon={actualPokemon} />}
+      >{`Go to ${name}'s details page`}</Link>
+    </div>
+  );
+};
 
 PokemonCard.propTypes = {
   name: PropTypes.string,
 };
+
+export default PokemonCard;
